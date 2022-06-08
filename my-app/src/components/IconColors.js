39000a108d71a829f55474a7860602b88cc95e4e
Krister Sepp <kristersepp@gmail.com>
2022-06-08 08:44:12 +0300

    change some colors

--- my-app/src/components/IconColors.js
+++ my-app/src/components/IconColors.js
@@ -5,14 +5,14 @@ const iconColorMediumShades = [
   '#66b032', // green
   '#ff8243', // orange
   '#ff69b4', // pink
-  '#6f2da8', // purple
+  '#600ca8', // purple
 ];
 
 const iconColorReds = [
   '#FA8072', '#db2131', '#8B0000', // reds
 ];
 const iconColorBlues = [
-  '#add8e6', '#0070ff', '#00008b', // blues
+  '#98d8fa', '#0070ff', '#00008b', // blues
 ];
 const iconColorGreens = [
   '#cae00d', '#66b032', '#1e4d2b', // greens
@@ -21,7 +21,7 @@ const iconColorOranges = [
   '#ffbf00', '#ff8243', '#8B4513', // oranges
 ];
 const iconColorPurples = [
-  '#ff69b4', '#bb3385', '#6f2da8'  // purples
+  '#ff69b4', '#b541d9', '#600ca8'  // purples
 ];
 
 const allSets = [
