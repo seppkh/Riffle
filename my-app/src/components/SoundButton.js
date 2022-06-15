f516454ffc0197d5bbec312c5a8d4fd2dd80581b
valpar <valmarparts@gmail.com>
2022-06-15 21:19:41 +0300

    Fixed sound button

--- my-app/src/components/SoundButton.js
+++ my-app/src/components/SoundButton.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './SoundButton.module.css';
-import { ReactComponent as IconSoundOff } from '../assets/sound-icon/mute.svg';
-import { ReactComponent as IconSoundOn } from '../assets/sound-icon/loud.svg';
+import logoSoundOn from '../assets/sound-icon/loud.png';
+import logoSoundOff from '../assets/sound-icon/mute.png';
 
 const SoundButton = ({ soundOn, onClick }) => {
   return (
     <button className={styles.soundBtn} onClick={onClick}>
-      {soundOn ? <IconSoundOn /> : <IconSoundOff />}
+      {soundOn ? (
+        <img src={logoSoundOn} alt='sound on' height='90px' />
+      ) : (
+        <img src={logoSoundOff} alt='sound off' height='90px' />
+      )}
     </button>
   );
 };
